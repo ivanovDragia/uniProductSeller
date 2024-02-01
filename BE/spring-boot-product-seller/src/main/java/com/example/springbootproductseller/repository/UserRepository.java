8594b457a0adcc905797a3cf228c567ee915422c
ivanovDragia <ivanovdragia1@gmail.com>
2024-02-01 20:15:59 +0200

    created user repo

---
+++ BE/spring-boot-product-seller/src/main/java/com/example/springbootproductseller/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.springbootproductseller.repository;
+
+import com.example.springbootproductseller.model.Role;
+import com.example.springbootproductseller.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Query("update User set role = :role where username = :username")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
+}
