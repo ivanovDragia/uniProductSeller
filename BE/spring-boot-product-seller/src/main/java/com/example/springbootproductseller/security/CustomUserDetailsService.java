6522fa16a4c73bff1498078a1d2fa4db16f33ab1
ivanovDragia <ivanovdragia1@gmail.com>
2024-02-02 00:18:44 +0200

    created customuserdetailsservice and userprincipe

---
+++ BE/spring-boot-product-seller/src/main/java/com/example/springbootproductseller/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.springbootproductseller.security;
+
+import com.example.springbootproductseller.model.User;
+import com.example.springbootproductseller.service.UserService;
+import com.example.springbootproductseller.utils.SecurityUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserService userService;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrinciple.builder()
+                .user(user)
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+
+    }
+}
