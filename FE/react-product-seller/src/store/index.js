4c52f0b25133cab90067cecda55aaea6c5cd40c7
ivanovDragia <ivanovdragia1@gmail.com>
2024-02-03 17:05:24 +0200

    hooked up sign up page

---
+++ FE/react-product-seller/src/store/index.js
@@ -0,0 +1,10 @@
+import { combineReducers, createStore } from "redux";
+import userReducer from "./reducers/user";
+
+const allReducers = combineReducers({
+  user: userReducer,
+});
+
+const store = createStore(allReducers);
+
+export default store;
